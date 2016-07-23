547f8640f43c23406ade6eeaa954cd132b777501
Jeremy Landis <jeremylandis@hotmail.com>
2016-07-23 00:02:30 -0400

    [code] Add supporting code/javadocs to allow easy deployment to sonatype

---
+++ src/main/java/com/github/grgrzybek/notice/TomcatSlf4jLogback.java
@@ -0,0 +1,11 @@
+package com.github.grgrzybek.notice;
+
+/**
+ * Tomcat SLF4J Logback is a drop in replacement to tomcat allowing full all
+ * internal logging to use our favorite slf4j/logback libraries.
+ *
+ * @see <a href="https://github.com/grgrzybek/tomcat-slf4j-logback">https://github.com/grgrzybek/tomcat-slf4j-logback</a>
+ */
+public class TomcatSlf4jLogback {
+    // Marker Class
+}
