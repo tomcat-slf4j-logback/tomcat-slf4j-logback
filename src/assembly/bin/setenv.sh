792e36978ee42887e15c478e1b989f38881896c6
Jeremy Landis <jeremylandis@hotmail.com>
2017-03-04 19:52:30 -0500

    [license] Add proper license, correct bad licenses, and overall license update

--- src/assembly/bin/setenv.sh
+++ src/assembly/bin/setenv.sh
@@ -1,1 +1,13 @@
+#
+# Tomcat-Slf4j-Logback (https://github.com/tomcat-slf4j-logback/tomcat-slf4j-logback/)
+#
+# Copyright (c) 2010-2017 Tomcat-Slf4j-Logback.
+#
+# All rights reserved. This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available
+# at https://www.eclipse.org/legal/epl-v10.html.
+#
+# Contributors: Tomcat-Slf4j-Logback Team.
+#
+
 JAVA_OPTS="$JAVA_OPTS -Djuli-logback.configurationFile=$CATALINA_HOME/conf/logback.xml"
